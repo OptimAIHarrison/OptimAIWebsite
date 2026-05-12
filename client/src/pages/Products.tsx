ade90b547c18a04659a14a84880318129bff3b24
OptimAIHarrison <hello@optimai.com.au>
2026-05-13 08:26:25 +1000

    Update Products.tsx

--- client/src/pages/Products.tsx
+++ client/src/pages/Products.tsx
@@ -991,7 +991,7 @@ export default function Products() {
                   </button>
                 </div>
               ) : (
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6">
                   {filteredProducts.map((product, idx) => (
                     <motion.div
                       key={product.id}
