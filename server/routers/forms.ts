f88656ae85258c5d2f6fab0f8b714ffcf1264a89
OptimAIHarrison <hello@optimai.com.au>
2026-05-12 11:53:51 +1000

    Update forms.ts

--- server/routers/forms.ts
+++ server/routers/forms.ts
@@ -1,7 +1,6 @@
 import { z } from "zod";
 import { publicProcedure, router } from "../_core/trpc";
 import { notifyOwner } from "../_core/notification";
-import { sendEmail } from "../_core/notification";
 
 export const formsRouter = router({
   submitContact: publicProcedure
